03324b5ca1b6340bc0f9151aceea10487b3430ab
mcornale <massimiliano.cornale@gmail.com>
2022-03-03 17:05:33 +0100

    Give a more generic name to formatDate parameter

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,10 +1,10 @@
-const formatDate = (paymentDueDate) =>
-  `${paymentDueDate
-    .getDate()
-    .toString()
-    .padStart(2, '0')} ${paymentDueDate.toLocaleDateString('en-EN', {
-    year: 'numeric',
-    month: 'short',
-  })}`;
+const formatDate = (date) =>
+  `${date.getDate().toString().padStart(2, '0')} ${date.toLocaleDateString(
+    'en-EN',
+    {
+      year: 'numeric',
+      month: 'short',
+    }
+  )}`;
 
 export default formatDate;
