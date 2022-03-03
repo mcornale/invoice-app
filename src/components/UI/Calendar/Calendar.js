918a1a15a89086f2a3158ee54744d109ad0116c8
mcornale <massimiliano.cornale@gmail.com>
2022-03-03 18:41:41 +0100

    Add InputDate and Calendar

---
+++ src/components/UI/Calendar/Calendar.js
@@ -0,0 +1,126 @@
+import { useState } from 'react';
+import formatDate from '../../../helpers/formatDate';
+import Icon from '../Icon/Icon';
+import styles from './Calendar.module.css';
+
+const MIN_DAY_NUMBER = 1;
+const MIN_MONTH_NUMBER = 0;
+const MAX_MONTH_NUMBER = 11;
+const DAYS_OF_THE_WEEK = 7;
+
+const Calendar = (props) => {
+  const { className, inputDate, onCalendarDayClick } = props;
+
+  const selectedDate = new Date(inputDate);
+
+  const [selectedYear, setSelectedYear] = useState(selectedDate.getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState(selectedDate.getMonth());
+
+  const selectedMonthDays = [];
+  const previousMonthDays = [];
+  const nextMonthDays = [];
+
+  const handleArrowLeftClick = () => {
+    if (selectedMonth - 1 < MIN_MONTH_NUMBER) {
+      setSelectedYear((prevSelectedYear) => --prevSelectedYear);
+      setSelectedMonth(MAX_MONTH_NUMBER);
+    } else setSelectedMonth((prevSelectedMonth) => --prevSelectedMonth);
+  };
+
+  const handleArrowRightClick = () => {
+    if (selectedMonth + 1 > MAX_MONTH_NUMBER) {
+      setSelectedYear((prevSelectedYear) => ++prevSelectedYear);
+      setSelectedMonth(MIN_MONTH_NUMBER);
+    } else setSelectedMonth((prevSelectedMonth) => ++prevSelectedMonth);
+  };
+
+  const populateDaysArray = (date, daysArr) => {
+    let dayToPush = date.getDate();
+    daysArr.push(dayToPush);
+    date.setDate(dayToPush + 1);
+  };
+
+  const generateCalendarDays = () => {
+    const date = new Date(selectedYear, selectedMonth, MIN_DAY_NUMBER);
+    const firstDayOfSelectedMonth = date.getDay();
+    let daysToFillAtStart = 0;
+
+    if (firstDayOfSelectedMonth === 0) daysToFillAtStart = DAYS_OF_THE_WEEK - 1;
+    if (firstDayOfSelectedMonth > 0)
+      daysToFillAtStart =
+        DAYS_OF_THE_WEEK - (DAYS_OF_THE_WEEK - firstDayOfSelectedMonth + 1);
+
+    date.setDate(date.getDate() - daysToFillAtStart);
+
+    while (daysToFillAtStart > 0) {
+      populateDaysArray(date, previousMonthDays);
+      daysToFillAtStart--;
+    }
+
+    while (date.getMonth() === selectedMonth) {
+      populateDaysArray(date, selectedMonthDays);
+    }
+
+    while (
+      (previousMonthDays.length +
+        selectedMonthDays.length +
+        nextMonthDays.length) %
+        DAYS_OF_THE_WEEK !==
+      0
+    ) {
+      populateDaysArray(date, nextMonthDays);
+    }
+  };
+
+  generateCalendarDays();
+
+  return (
+    <div className={`${className} ${styles.calendar}`}>
+      <div className={styles.calendarHeader}>
+        <button
+          onClick={handleArrowLeftClick}
+          className={styles.calendarHeaderButton}
+        >
+          <Icon icon='arrowLeft' />
+        </button>
+        <p>
+          {formatDate(
+            new Date(selectedYear, selectedMonth, MIN_DAY_NUMBER)
+          ).slice(2)}
+        </p>
+        <button
+          onClick={handleArrowRightClick}
+          className={styles.calendarHeaderButton}
+        >
+          <Icon icon='arrowRight' />
+        </button>
+      </div>
+      <div className={styles.calendarDays}>
+        {previousMonthDays.map((day, index) => (
+          <button className={styles.calendarDayHidden} key={index}>
+            {day}
+          </button>
+        ))}
+        {selectedMonthDays.map((day, index) => (
+          <button
+            onClick={onCalendarDayClick.bind(
+              null,
+              new Date(selectedYear, selectedMonth, day)
+            )}
+            className={styles.calendarDay}
+            key={index}
+          >
+            {day}
+          </button>
+        ))}
+        {nextMonthDays.map((day, index) => (
+          <button className={styles.calendarDayHidden} key={index}>
+            {day}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
--- src/components/UI/InputDate/InputDate.js
+++ src/components/UI/InputDate/InputDate.js
@@ -1,13 +1,40 @@
-import { forwardRef, useState } from 'react';
+import { useState } from 'react';
+import formatDate from '../../../helpers/formatDate';
+import Calendar from '../Calendar/Calendar';
+import Icon from '../Icon/Icon';
 
 import styles from './InputDate.module.css';
 
-const InputDate = forwardRef((props, ref) => {
-  const { value, className } = props;
+const InputDate = (props) => {
+  const { value, className, readOnly } = props;
 
-  const [inputValue, setInputValue] = useState(value);
+  const [inpuDateValue, setInputDateValue] = useState(formatDate(value));
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
-  return <p className={className}>Date</p>;
-});
+  const handleInputDateClick = () => {
+    setIsCalendarOpen((prevCalendarState) => !prevCalendarState);
+  };
+
+  const handleCalendarDayClick = (newDate) => {
+    setInputDateValue(formatDate(newDate));
+    setIsCalendarOpen(false);
+  };
+
+  return (
+    <div className={`${className} ${styles.inputDateContainer}`}>
+      <button onClick={handleInputDateClick} className={styles.inputDate}>
+        <p>{inpuDateValue}</p>
+        <Icon icon='calendar' />
+      </button>
+      {isCalendarOpen && (
+        <Calendar
+          onCalendarDayClick={handleCalendarDayClick}
+          className={styles.inputDateCalendar}
+          inputDate={value}
+        />
+      )}
+    </div>
+  );
+};
 
 export default InputDate;
