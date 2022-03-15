786c9e846d75ab213a222327b8ff6841afbc78c4
mcornale <massimiliano.cornale@gmail.com>
2022-03-15 15:48:28 +0100

    Reorganize css variables

--- src/components/Invoices/DeleteInvoice/DeleteInvoice.js
+++ src/components/Invoices/DeleteInvoice/DeleteInvoice.js
@@ -1,5 +1,6 @@
 import styles from './DeleteInvoice.module.css';
 import Button from '../../UI/Button/Button';
+import Container from '../../UI/Container/Container';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { deleteInvoice } from '../../../store/invoicesSlice';
@@ -20,19 +21,21 @@ const DeleteInvoice = (props) => {
 
   return (
     <div className={styles.deleteInvoice}>
-      <h2 className={styles.deleteInvoiceTitle}>Confirm Deletion</h2>
-      <p className={styles.deleteInvoiceMessage}>
-        Are you sure you want to delete invoice #{currentInvoiceId}? This action
-        cannot be undone.
-      </p>
-      <div className={styles.deleteInvoiceActions}>
-        <Button onClick={handleCancelBtnClick} buttonStyle='2'>
-          Cancel
-        </Button>
-        <Button onClick={handleDeleteBtnClick} buttonStyle='4'>
-          Delete
-        </Button>
-      </div>
+      <Container>
+        <h2 className={styles.deleteInvoiceTitle}>Confirm Deletion</h2>
+        <p className={styles.deleteInvoiceMessage}>
+          Are you sure you want to delete invoice #{currentInvoiceId}? This
+          action cannot be undone.
+        </p>
+        <div className={styles.deleteInvoiceActions}>
+          <Button onClick={handleCancelBtnClick} buttonStyle='2'>
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteBtnClick} buttonStyle='4'>
+            Delete
+          </Button>
+        </div>
+      </Container>
     </div>
   );
 };
