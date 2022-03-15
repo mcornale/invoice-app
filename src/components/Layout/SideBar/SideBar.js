9a5d76a2cb93161b686378673d741f00a5f68874
mcornale <massimiliano.cornale@gmail.com>
2022-03-15 11:26:03 +0100

    Add dark theme

--- src/components/Layout/SideBar/SideBar.js
+++ src/components/Layout/SideBar/SideBar.js
@@ -2,8 +2,17 @@ import logoImgSrc from '../../../assets/images/logo.svg';
 import profileImgSrc from '../../../assets/images/image-avatar.jpg';
 import styles from './SideBar.module.css';
 import Icon from '../../UI/Icon';
+import { useState } from 'react';
 
 const Sidebar = () => {
+  const [theme, setTheme] = useState(document.body.dataset.theme);
+
+  const handleChangeThemeBtn = () => {
+    document.body.dataset.theme =
+      document.body.dataset.theme === 'light' ? 'dark' : 'light';
+    setTheme(document.body.dataset.theme);
+  };
+
   return (
     <aside className={styles.sidebar}>
       <div className={styles.sidebarLogo}>
@@ -14,8 +23,8 @@ const Sidebar = () => {
         />
       </div>
       <div className={styles.sidebarElements}>
-        <button className={styles.sidebarThemeSwitch}>
-          <Icon icon='moon' />
+        <button onClick={handleChangeThemeBtn}>
+          <Icon icon={theme === 'light' ? 'moon' : 'sun'} />
         </button>
         <img
           className={styles.sidebarProfileImg}
