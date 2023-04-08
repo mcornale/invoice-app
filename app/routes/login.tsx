1612903a85b07f253d7f30d5394e3f5825eb8c36
Massimiliano Cornale <massimiliano.cornale@gmail.com>
2023-04-08 23:09:52 +0200

    improve structure of login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -7,41 +7,45 @@ export const links: LinksFunction = () => {
 
 export default function LoginRoute() {
   return (
-    <main className='main'>
+    <div className='container'>
       <header className='header'>
-        <img src='logo.svg' alt='logo' />
+        <img src='logo.svg' alt='logo' aria-hidden />
         <h1 className='text--3xl text--semibold'>Invoice App</h1>
       </header>
-      <form className='form'>
-        <div>
-          <label htmlFor='email'>
-            Email
-            <input
-              id='email'
-              type='email'
-              name='email'
-              placeholder='Enter your email'
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor='password'>
-            Password
-            <input
-              id='password'
-              type='password'
-              name='password'
-              placeholder='••••••••'
-              required
-            />
-          </label>
-        </div>
-        <div className='form__button-group'>
-          <button className='button--lg button--primary'>Login</button>
-          <button className='button--lg button--secondary-gray'>Sign Up</button>
-        </div>
-      </form>
-    </main>
+      <main className='main'>
+        <form className='form'>
+          <div>
+            <label>
+              Email
+              <input
+                type='email'
+                name='email'
+                placeholder='Enter your email'
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password
+              <input
+                type='password'
+                name='password'
+                placeholder='••••••••'
+                required
+              />
+            </label>
+          </div>
+          <div className='form__button-group'>
+            <button type='submit' className='button--lg button--primary'>
+              Login
+            </button>
+            <button type='submit' className='button--lg button--secondary-gray'>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
