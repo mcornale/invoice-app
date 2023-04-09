365ff3983b17e0c6780eddc611eef8cbfd541356
Massimiliano Cornale <massimiliano.cornale@gmail.com>
2023-04-09 20:56:55 +0200

    add dark mode support

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className='dark-mode'>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
