94946b85847afeaada4e1b0ff0308429300c5dab
Massimiliano Cornale <massimiliano.cornale@gmail.com>
2023-04-15 21:21:03 +0200

    minor fixes

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,6 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1' />
-        <link rel='icon' type='image/png' href='favicon.png' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
--- app/utils/helpers/upper-first.ts
+++ app/utils/helpers/upper-first.ts
@@ -1,2 +1,2 @@
 export const upperFirst = (word: string) =>
-  word[0].toUpperCase() + word.slice(1);
+  word[0].toUpperCase() + word.slice(1).toLowerCase();
