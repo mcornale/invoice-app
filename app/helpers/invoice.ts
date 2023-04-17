98f9b03ce294a5e4799faa678b45ed7d88511e77
Massimiliano Cornale <massimiliano.cornale@gmail.com>
2023-04-17 16:42:16 +0200

    use helper to simplify a bit the code

--- app/helpers/invoice.ts
+++ app/helpers/invoice.ts
@@ -1,7 +1,71 @@
+import { isArrOfString, isString } from '~/utils/type-checkers';
+
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const NUMBERS = '0123456789';
 
-export function generateDisplayId() {
+export function getTypedFormData(formData: FormData) {
+  const senderAddressStreet = formData.get('sender-address-street');
+  const senderAddressCity = formData.get('sender-address-city');
+  const senderAddressPostCode = formData.get('sender-address-post-code');
+  const senderAddressCountry = formData.get('sender-address-country');
+  const clientName = formData.get('client-name');
+  const clientEmail = formData.get('client-email');
+  const clientAddressStreet = formData.get('client-address-street');
+  const clientAddressCity = formData.get('client-address-city');
+  const clientAddressPostCode = formData.get('client-address-post-code');
+  const clientAddressCountry = formData.get('client-address-country');
+  const createdAt = formData.get('created-at');
+  const paymentTerms = formData.get('payment-terms');
+  const description = formData.get('description');
+  const itemNames = formData.getAll('item-name');
+  const itemQuantities = formData.getAll('item-quantity');
+  const itemPrices = formData.getAll('item-price');
+  const itemTotals = formData.getAll('item-total');
+
+  if (
+    !isString(senderAddressStreet) ||
+    !isString(senderAddressCity) ||
+    !isString(senderAddressPostCode) ||
+    !isString(senderAddressCountry) ||
+    !isString(clientName) ||
+    !isString(clientEmail) ||
+    !isString(clientAddressStreet) ||
+    !isString(clientAddressCity) ||
+    !isString(clientAddressPostCode) ||
+    !isString(clientAddressCountry) ||
+    !isString(createdAt) ||
+    !isString(paymentTerms) ||
+    !isString(description) ||
+    !isArrOfString(itemNames) ||
+    !isArrOfString(itemQuantities) ||
+    !isArrOfString(itemPrices) ||
+    !isArrOfString(itemTotals)
+  ) {
+    return;
+  }
+
+  return {
+    senderAddressStreet,
+    senderAddressCity,
+    senderAddressPostCode,
+    senderAddressCountry,
+    clientName,
+    clientEmail,
+    clientAddressStreet,
+    clientAddressCity,
+    clientAddressPostCode,
+    clientAddressCountry,
+    createdAt,
+    paymentTerms,
+    description,
+    itemNames,
+    itemQuantities,
+    itemPrices,
+    itemTotals,
+  };
+}
+
+export function getDisplayId() {
   const randomLetters =
     ALPHABET[Math.floor(Math.random() * ALPHABET.length)] +
     ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
@@ -19,3 +83,16 @@ export const getPaymentDue = (createdAt: Date, paymentTerms: number) => {
   const paymentTermsInMS = paymentTerms * 24 * 60 * 60 * 1000;
   return new Date(createdAt.getTime() + paymentTermsInMS);
 };
+
+export const getItems = (items: {
+  itemNames: string[];
+  itemQuantities: number[];
+  itemPrices: number[];
+  itemTotals: number[];
+}) =>
+  new Array(items.itemNames.length).fill(null).map((_, i) => ({
+    name: items.itemNames[i],
+    quantity: items.itemQuantities[i],
+    price: items.itemPrices[i],
+    total: items.itemTotals[i],
+  }));
--- app/routes/_app.invoices.new/index.tsx
+++ app/routes/_app.invoices.new/index.tsx
@@ -13,7 +13,6 @@ import type { ActionArgs, LinksFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import styles from './styles.css';
-import { isArrOfString, isString } from '~/utils/type-checkers';
 import {
   validateClientAddressCity,
   validateClientAddressCountry,
@@ -35,7 +34,12 @@ import {
 } from '~/utils/validators';
 import { Button } from '~/components/ui/button';
 import { useActionData, useNavigate } from '@remix-run/react';
-import { generateDisplayId, getPaymentDue } from '~/helpers/invoice';
+import {
+  getDisplayId,
+  getItems,
+  getPaymentDue,
+  getTypedFormData,
+} from '~/helpers/invoice';
 import { db } from '~/utils/db.server';
 import { InvoiceStatus } from '@prisma/client';
 import { parseDate } from '~/utils/parsers';
@@ -60,43 +64,8 @@ export const action = async ({ request }: ActionArgs) => {
   const formData = await request.formData();
   const intent = formData.get('intent');
 
-  const senderAddressStreet = formData.get('sender-address-street');
-  const senderAddressCity = formData.get('sender-address-city');
-  const senderAddressPostCode = formData.get('sender-address-post-code');
-  const senderAddressCountry = formData.get('sender-address-country');
-  const clientName = formData.get('client-name');
-  const clientEmail = formData.get('client-email');
-  const clientAddressStreet = formData.get('client-address-street');
-  const clientAddressCity = formData.get('client-address-city');
-  const clientAddressPostCode = formData.get('client-address-post-code');
-  const clientAddressCountry = formData.get('client-address-country');
-  const createdAtString = formData.get('created-at');
-  const paymentTermsString = formData.get('payment-terms');
-  const description = formData.get('description');
-  const itemNames = formData.getAll('item-name');
-  const itemQuantitiesString = formData.getAll('item-quantity');
-  const itemPricesString = formData.getAll('item-price');
-  const itemTotalsString = formData.getAll('item-total');
-
-  if (
-    !isString(senderAddressStreet) ||
-    !isString(senderAddressCity) ||
-    !isString(senderAddressPostCode) ||
-    !isString(senderAddressCountry) ||
-    !isString(clientName) ||
-    !isString(clientEmail) ||
-    !isString(clientAddressStreet) ||
-    !isString(clientAddressCity) ||
-    !isString(clientAddressPostCode) ||
-    !isString(clientAddressCountry) ||
-    !isString(createdAtString) ||
-    !isString(paymentTermsString) ||
-    !isString(description) ||
-    !isArrOfString(itemNames) ||
-    !isArrOfString(itemQuantitiesString) ||
-    !isArrOfString(itemPricesString) ||
-    !isArrOfString(itemTotalsString)
-  ) {
+  const typedFormData = getTypedFormData(formData);
+  if (!typedFormData) {
     return json<ActionData>(
       {
         fieldErrors: null,
@@ -106,40 +75,45 @@ export const action = async ({ request }: ActionArgs) => {
     );
   }
 
-  const displayId = generateDisplayId();
-  const paymentTerms = Number(paymentTermsString);
-  const itemQuantities = itemQuantitiesString.map((qty) => Number(qty));
-  const itemPrices = itemPricesString.map((price) => Number(price));
-  const itemTotals = itemTotalsString.map((total) => Number(total));
-  const createdAt = parseDate(createdAtString);
+  const displayId = getDisplayId();
+  const senderAddress = {
+    street: typedFormData.senderAddressStreet,
+    city: typedFormData.senderAddressCity,
+    postCode: typedFormData.senderAddressPostCode,
+    country: typedFormData.senderAddressCountry,
+  };
+  const clientName = typedFormData.clientName;
+  const clientEmail = typedFormData.clientEmail;
+  const clientAddress = {
+    street: typedFormData.clientAddressStreet,
+    city: typedFormData.clientAddressCity,
+    postCode: typedFormData.clientAddressPostCode,
+    country: typedFormData.clientAddressCountry,
+  };
+  const paymentTerms = Number(typedFormData.paymentTerms);
+  const description = typedFormData.description;
+
+  const itemNames = typedFormData.itemNames;
+  const itemQuantities = typedFormData.itemQuantities?.map((qty) =>
+    Number(qty)
+  );
+  const itemPrices = typedFormData.itemPrices.map((price) => Number(price));
+  const itemTotals = typedFormData.itemTotals.map((total) => Number(total));
+
+  const createdAt = parseDate(typedFormData.createdAt);
   const paymentDue =
     createdAt instanceof Date ? getPaymentDue(createdAt, paymentTerms) : null;
-  const items = new Array(itemNames.length).fill(null).map((_, i) => ({
-    name: itemNames[i],
-    quantity: itemQuantities[i],
-    price: itemPrices[i],
-    total: itemTotals[i],
-  }));
+  const items = getItems({ itemNames, itemQuantities, itemPrices, itemTotals });
 
   switch (intent) {
     case 'save-as-draft':
       await db.invoice.create({
         data: {
           displayId,
-          senderAddress: {
-            street: senderAddressStreet,
-            city: senderAddressCity,
-            postCode: senderAddressPostCode,
-            country: senderAddressCountry,
-          },
+          senderAddress,
           clientName,
           clientEmail,
-          clientAddress: {
-            street: clientAddressStreet,
-            city: clientAddressCity,
-            postCode: clientAddressPostCode,
-            country: clientAddressCountry,
-          },
+          clientAddress,
           createdAt,
           paymentTerms,
           paymentDue,
@@ -151,6 +125,43 @@ export const action = async ({ request }: ActionArgs) => {
       });
       return redirect(`/invoices`);
     case 'save-and-send':
+      const fieldErrors: ActionData['fieldErrors'] = {
+        senderAddressStreet: validateSenderAddressStreet(senderAddress.street),
+        senderAddressCity: validateSenderAddressCity(senderAddress.city),
+        senderAddressPostCode: validateSenderAddressPostCode(
+          senderAddress.postCode
+        ),
+        senderAddressCountry: validateSenderAddressCountry(
+          senderAddress.country
+        ),
+        clientName: validateClientName(clientName),
+        clientEmail: validateClientEmail(clientEmail),
+        clientAddressStreet: validateClientAddressStreet(clientAddress.street),
+        clientAddressCity: validateClientAddressCity(clientAddress.city),
+        clientAddressPostCode: validateClientAddressPostCode(
+          clientAddress.postCode
+        ),
+        clientAddressCountry: validateClientAddressCountry(
+          clientAddress.country
+        ),
+        createdAt: validateCreatedAt(createdAt),
+        paymentTerms: validatePaymentTerms(paymentTerms),
+        description: validateDescription(description),
+        itemNames: validateItemNames(itemNames),
+        itemQuantities: validateItemQuantities(itemQuantities),
+        itemPrices: validateItemPrices(itemPrices),
+        itemTotals: validateItemTotals(itemTotals),
+      };
+
+      if (Object.values(fieldErrors).some(Boolean)) {
+        return json<ActionData>(
+          {
+            fieldErrors,
+            formErrors: null,
+          },
+          { status: 400 }
+        );
+      }
       return;
     default:
       return new Response(`Unsupported intent: ${intent}`, { status: 400 });
