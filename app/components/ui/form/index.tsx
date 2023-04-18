59345b479db248be8a3c1544e525f989496af33b
Massimiliano Cornale <massimiliano.cornale@gmail.com>
2023-04-18 07:08:42 +0200

    add form group comp

--- app/components/ui/form/index.tsx
+++ app/components/ui/form/index.tsx
@@ -2,9 +2,11 @@ import type { LinksFunction } from '@remix-run/node';
 import styles from './styles.css';
 import type { FormProps as RemixFormProps } from '@remix-run/react';
 import { Form as RemixForm } from '@remix-run/react';
+import type { HTMLAttributes } from 'react';
 import { forwardRef } from 'react';
 
 export interface FormProps extends RemixFormProps {}
+export interface FormGroupProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const links: LinksFunction = () => {
   return [
@@ -25,3 +27,14 @@ export const Form = forwardRef<HTMLFormElement, FormProps>(
   }
 );
 Form.displayName = 'Form';
+
+export const FormGroup = forwardRef<HTMLDivElement, FormGroupProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <div className='form-group' {...props} ref={ref}>
+        {children}
+      </div>
+    );
+  }
+);
+FormGroup.displayName = 'FormGroup';
